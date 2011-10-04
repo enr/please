d9ade8de6c3aef25aed8889e45bec2089c857218
enr <enrico.comiti@gmail.com>
2011-10-04 23:44:42 +0200

    added license headers to source files

--- modules/core/src/main/groovy/com/atoito/please/core/util/Directories.java
+++ modules/core/src/main/groovy/com/atoito/please/core/util/Directories.java
@@ -1,3 +1,22 @@
+/* License added by: GRADLE-LICENSE-PLUGIN
+ *
+ * 
+ * Copyright (C) 2011 - https://github.com/enr
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 package com.atoito.please.core.util;
 
 //Here is a simple utility which returns a sorted List of File objects which reside under a given root directory. It uses recursion.
@@ -169,3 +188,4 @@ public final class Directories
         }
     }
 }
+
